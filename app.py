87e8910b9218d761531bce335af8f31b5862de1e
cristiandpto23 <95114277+cristiandpto23@users.noreply.github.com>
2026-07-18 13:17:49 -0400

    Implement document chunking

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from src.document_loader import load_documents
+from src.text_splitter import split_documents
 
 documents = load_documents("data/pdfs")
 
-print(f"Se cargaron {len(documents)} páginas.\n")
+chunks = split_documents(documents)
 
-print("Metadata:")
-print(documents[0].metadata)
+print(f"Documentos cargados: {len(documents)}")
+print(f"Chunks creados: {len(chunks)}")
 
-print("\nContenido:")
-print(documents[0].page_content[:500])
+print("\nPrimer chunk:\n")
+
+print(chunks[0].page_content)
